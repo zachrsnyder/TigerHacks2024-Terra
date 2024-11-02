586fce2e0f2a63f64ef5b55f0cb126302b22290b
zachrsnyder <zachrsnyder2004@gmail.com>
2024-11-02 16:02:19 -0500

    Changed cursor type

--- client/src/components/LeftBar/PlotCard.js
+++ client/src/components/LeftBar/PlotCard.js
@@ -7,7 +7,7 @@ const PlotCard = ({data}) => {
 
 
   return (
-    <div className=' rounded-md shadow-md hover:bg-primary-hover mx-2 my-[.1rem] bg-primary' onClick={handleClick}>
+    <div className=' rounded-md shadow-md hover:bg-primary-hover mx-2 my-[.1rem] bg-primary hover:cursor-pointer' onClick={handleClick}>
         <p className='text-xl'>{data.name}</p>
     </div>
   )
