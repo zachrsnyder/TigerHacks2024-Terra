ca0c90721e1720ebe4657196b420e291c6f5df28
aaron-yang799 <aaron.yang799@gmail.com>
2024-11-02 18:14:28 -0500

    Made left dashboard eaiser to see

--- client/src/components/LeftBar/LeftDashboard.js
+++ client/src/components/LeftBar/LeftDashboard.js
@@ -22,7 +22,7 @@ const LeftDashboard = ({pointPlots, setPointPlots, selectedPlot, setSelectedPlot
         <PlotSection pointPlots={pointPlots} setSelectedPlot={setSelectedPlot} selectedPlot={selectedPlot}/>
       </aside>
       <div className='h-full items-center relative flex justify-center'>
-        <div className="w-[2vw] h-[6vw] bg-primary rounded-r-full justify-center align-middle items-center flex z-10" style={{ opacity: isHovered ? .70 : .30, color: 'white' }} onClick={toggleAside} onMouseEnter={() => setIsHovered(true)}
+        <div className="w-[2vw] h-[6vw] bg-primary rounded-r-full justify-center align-middle items-center flex z-10" style={{ opacity: isHovered ? .90 : .75, color: 'white' }} onClick={toggleAside} onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}>
           {isCollapsed ? (<ChevronRight color='white' size={24}/>) : (<ChevronLeft color='white' size={24}/>) }
         </div>
